2c4e0d05866861ad23b637809abc415dc7b741dd
Fang Jian <fangjianmail@126.com>
2016-11-03 15:54:11 +0800

    cors

--- app.js
+++ app.js
@@ -11,9 +11,8 @@ var online={}; //在线状态
 //设置跨域访问  
 app.all('*', function(req, res, next) {  
     res.header("Access-Control-Allow-Origin", "*");  
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");  
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");  
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");  
-    res.header("Content-Type", "application/json;charset=utf-8");  
     next();  
 }); 
 
